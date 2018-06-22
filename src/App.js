71965e6dfc2f5be53a892a072b21e520301bfe52
Sibyl Dawn Pomeroy <Sibyl@Sibyls-MacBook-Air.local>
2018-06-22 14:50:38 -0700

    Add Compartments

--- src/App.js
+++ src/App.js
@@ -2,9 +2,53 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      picture:'I am a picture.',
+      name:'Sibyl',
+      friends: [{
+        name: 'My 404 friend',
+        picture: 'https://http.cat/404'
+      },{
+        name: 'My chill cat',
+        picture: 'https://http.cat/420'
+      }],
+    }
+  }
+
+  changeName(event) {
+     console.log(event.target.value)
+     this.setState ({
+     name: event.target.value
+   })
+  }
+
+  changePicture(event) {
+    console.log(event.target.value)
+    this.setState ({
+    picture: event.target.value
+  })
+ }
+
+
   render() {
     return (
-      <div></div>
+      <div>
+        Name:<input value={this.state.name} onChange= {(event) => this.changeName(event)} />
+        Picture:<input value={this.state.picture} onChange= {(event) => this.changePicture(event)} />
+        <div>
+          {this.state.friends.map(friend => {
+          console.log(friend)
+           return (
+           <div>
+            <p> <img src = {friend.picture} alt ={friend.name}/> </p>
+             <p><b>{friend.name}</b></p>
+           </div>
+         )
+        })}
+        </div>
+      </div>
     );
   }
 }
